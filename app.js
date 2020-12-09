af7e82c535f1b9d0e480259669a3a149090c69e4
Marco <marco@empleosti.com.mx>
2020-12-09 14:42:39 -0600

    update olatio

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Ola tio que haces!'),
     };
     return response;
 };                 
